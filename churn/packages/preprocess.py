8b0e0c089817c843ec431577930078d3db7c9d34
daniel gusko <daniel.h.gusko@gmail.com>
2024-07-01 21:17:23 +0100

    Finished the preprocessing pipeline

--- churn/packages/preprocess.py
+++ churn/packages/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from colorama import Fore, Style
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
